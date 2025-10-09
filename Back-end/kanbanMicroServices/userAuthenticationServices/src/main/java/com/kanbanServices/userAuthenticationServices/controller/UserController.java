b39bb6b98b31aaaca6363cd17220d10a357d5d01
roopal negi <roopalnegi147@gmail.comgit config --global user.name roopal>
2025-10-09 08:20:07 +0530

    structure change

--- Back-end/kanbanMicroServices/userAuthenticationServices/src/main/java/com/kanbanServices/userAuthenticationServices/controller/UserController.java
+++ Back-end/kanbanMicroServices/userAuthenticationServices/src/main/java/com/kanbanServices/userAuthenticationServices/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3030")
+@CrossOrigin(origins = "http://localhost:3001")
 @RequestMapping("/api/v1/user")
 public class UserController
 {
