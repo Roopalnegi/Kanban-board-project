94d34111ac53fdf4f77a1e82cf0d24df4bedd7d8
saloni <salonishrivastva2003@gmail.com>
2025-10-11 22:24:48 +0530

    saloni

--- Front-end/kanban-board-project/src/Forms/Login/LoginForm.jsx
+++ Front-end/kanban-board-project/src/Forms/Login/LoginForm.jsx
@@ -1,6 +1,157 @@
-function LoginForm()
-{
-  return <h2>Login form </h2>
-}
+import { useState } from "react";
+import {useForm}from 'react-hook-form';
+import{useNavigate}from 'react-router-dom';
+import { useSnackbar} from 'notistack';
+import axios from 'axios';
+import{
+        Dialog, DialogContent, DialogTitle, TextField,DialogActions,
+        useTheme,Button} from "@mui/material";
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+
+function LoginForm({setLoginStatus}){
+  const theme=useTheme();
+
+  const navigate=useNavigate();
+
+ const { enqueueSnackbar } = useSnackbar();
+
+
+  const[formOpen,setFormOpen]=useState(true);// controls visibility of login
+  
+  //intialize from handling
+  const{
+      register,
+      handleSubmit,
+      formState:{errors},
+      trigger,
+      reset
+  }=useForm();
+
+  //handle login from submission
+  const  loginSubmit=async(useData)=>{
+    try
+    {
+      //call backend login api
+      const response=await axios.post("http://localhost:8081/api/v1/user/login",useData);
+      //get jwt token from response
+
+      const {token,message}=response.data;
+
+      //store token in localstorage
+      localStorage.setItem("token",token);
+
+      //update login status in app
+      setLoginStatus(true);
+
+      //show sucess message
+      enqueueSnackbar(message||"login successfully",{
+                                                    variant:"success",
+                                                    autoHideDuration:2000, // redirect after 2 sec
+                                                    anchorOrigin:
+                                                    {
+                                                      vertical:"top",
+                                                      horizontal:'right'
+                                                    },
+                                                  });
+      //close login form and redirect to dashboard
+      setFormOpen(false);
+      navigate('/');
+    }catch(error)
+    {
+      //show error message
+      enqueueSnackbar(
+        error.response?.data||"login failed please check credentials",{
+                                                                        variant:'error',
+                                                                        autoHideDuration:2000,
+                                                                        anchorOrigin:
+                                                                        {
+                                                                          vertical:"top",
+                                                                          horizontal:"right"
+                                                                        },
+                                                                  
+                                                                      }
+      );
+    }
+  };
+
+    return(
+      <Dialog open={formOpen} onClose={()=>setFormOpen(false)} fullWidth maxWidth='sm'>
+        {/*header of the dialog */}
+        <DialogTitle 
+          sx=
+          {{display:'flex',justifyContent:'space-between',alignItems:"center"}}>
+            <b>Login form</b>
+              {/* closeIcon */}
+          <CancelIcon
+                sx={{cursor:'pointer',color:theme.palette.error.main}}
+                onClick={()=>setFormOpen(false)}
+          />
+        </DialogTitle>
+
+{/*login form */}
+          <form onSubmit={handleSubmit( loginSubmit)}>
+            <DialogContent sx={{display:'flex',flexDirection:"column",gap:"20px"}}>
+
+              {/* usernaneFeild */}
+              <TextField label="Enter userName"
+              variant="outlined"
+              fullWidth
+              {...register("username",{ required: {value: true, message: "Username is required"},
+                                             pattern:  {value : /^[A-Za-z][A-Za-z0-9_]*$/, message: "Username must start with letter"},
+                                           })}
+              onBlur={()=>trigger('userName')}
+              error={!!errors.useForm?.message}
+              helperText={errors.userName?.message}
+              />
+              {/* EmailFeild */}
+              <TextField
+              label="Enter Email"
+              variant="outlined"
+              fullWidth
+              {...register("email",{ required: {value: true, message: "Email Id is required"},
+                                          pattern: {value : /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Email must be in correct format"},
+                                        })}
+              onBlur={()=>trigger('email')}
+              error={!!errors.email?.message}
+              helperText={errors.email?.message}
+              />
+
+              {/* passwordFeild */}
+              <TextField
+              label="Enter password"
+              type="password"
+              variant="outlined"
+              fullWidth
+              {...register('password',{
+                required:"password is required",
+                minLength:{
+                  value:8,
+                  message:"Password must be at least 8 character"
+                }
+              })}
+              onBlur={()=>trigger('password')}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              />
+            </DialogContent>
+
+ 
+            {/*button-sumbit and reset */}
+
+            <DialogActions sx={{ my: 2 }}>
+          <Button type="submit" variant="contained" sx={{ backgroundColor: theme.palette.primary.main }}>
+            Login
+          </Button>
+          <Button type="reset" variant="contained" onClick={() => reset()} sx={{ backgroundColor: theme.palette.secondary.main }}>
+            Reset
+          </Button>
+        </DialogActions>
+        </form>
+      </Dialog>
+    )
+  }
+
 
 export default LoginForm;
